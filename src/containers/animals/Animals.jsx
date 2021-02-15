4f3cf9a2bc18408cf57b48bc0fc0d0f7fb198fd7
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-15 09:00:59 -0500

    construct custom hook

--- src/containers/animals/Animals.jsx
+++ src/containers/animals/Animals.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Loading from '../../components/loading/Loading';
 import Request from '../../components/request/Request';
 
-export default function animals() {
+export default function Animals() {
   // currently false, but will update later
   const [loading, setLoading] = useState(false);
   
---
+++ src/services/fetchServer.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+const URL = 'https://secret-peak-24272.herokuapp.com/api/v1/animals';
+
+export const postAnimal = (name, type, characteristic) => {
+  const data = { name, type, characteristic };
+    
+  return fetch(`${URL}`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' }
+  }).then(res => res.json())
+    .then(json => console.log(json));
+};
+
